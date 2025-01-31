a6bfab5bbc36a16c6a44116096e1265262079b62
Павлов Иван Иванович <w3dipmail@gmail.com>
2025-01-31 19:31:53 +0300

    some changes

--- utils/files.py
+++ utils/files.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-def save_data_to_file(_data, path, _symbol, _timeframe):
+def save_data_to_file(_data, _path, _symbol, _timeframe):
     _data.columns = ['date','open','high','low','close','volume']
     _data = _data.sort_values(by='date')
     _data = _data.drop_duplicates(subset='date').reset_index(drop=True)
     _data['date'] = pd.to_datetime(_data['date'], unit='ms')
-    _data.to_csv(path + _symbol.replace('/', '_') + '_' + _timeframe + '.csv', index=False)
+    _data.to_csv(_path + _symbol.replace('/', '_') + '_' + _timeframe + '.csv', index=False)
 
-def read_data_from_file(path, _symbol, _timeframe):
-    _df = pd.read_csv(path + _symbol.replace('/', '_') + '_' + _timeframe + '.csv')
+def read_data_from_file(_path, _symbol, _timeframe):
+    _df = pd.read_csv(_path + _symbol.replace('/', '_') + '_' + _timeframe + '.csv')
     _df = _df.sort_values(by='date')
     _df = _df.drop_duplicates(subset='date').reset_index(drop=True)
     _df['date'] = pd.to_datetime(_df['date'])
